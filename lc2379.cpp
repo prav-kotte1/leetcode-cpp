d721ac5cf0bcd891d4c635a3b02e06fb566aaa9e
Pravallika Kotte <146621306+prav-kotte1@users.noreply.github.com>
2025-03-10 09:41:57 +0530

    Mar 6th to 10th

---
+++ lc2379.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int minimumRecolors(string blocks, int k) {
+            queue<char> blockQueue;
+            int numWhites = 0;
+    
+            while (blockQueue.size() < k) {
+                char currentChar = blocks[blockQueue.size()];
+                if (currentChar == 'W') numWhites++;
+                blockQueue.push(currentChar);
+            }
+    
+      
+            int numRecolors = numWhites;
+            for (int i = k; i < blocks.size(); i++) {
+                if (blockQueue.front() == 'W') numWhites--;
+                blockQueue.pop();
+                if (blocks[i] == 'W') numWhites++;
+                blockQueue.push(blocks[i]);
+    
+                numRecolors = min(numRecolors, numWhites);
+            }
+            return numRecolors;
+        }
+    };
